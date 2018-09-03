d7348024ee9203947f39caef85104335ffe8df2c
IliaSidash <iliasidash@gmail.com>
2018-09-03 22:45:55 +0500

    add Main

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import Main from './Main/';
 
 class App extends Component {
   render() {
-    return <div>Hello, PIK-arenda</div>;
+    return <Main />;
   }
 }
 
---
+++ src/Main/index.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+import Header from './Header/';
+import Text from './Text';
+import Form from './Form/';
+
+export default () => (
+  <Fragment>
+    <Header />
+    <Text />
+    <Form />
+  </Fragment>
+);
