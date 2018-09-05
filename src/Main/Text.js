520abfa5eda29bfcdc23d6e46a407832a1967325
iliasidash <iliasidash@gmail.com>
2018-09-05 12:43:21 +0500

    edit Main

--- src/Main/Text.js
+++ src/Main/Text.js
@@ -1,41 +1,74 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import media from '../style-utils';
 import Question from '../ui/Question';
 
 const Text = styled.div`
-  text-align: center;
+  padding: 0 30px;
+  font-family: 'GraphikLCG-Medium';
+
+  ${media.sm`
+    font-family: 'GraphikLCG-Regular';
+    text-align: center;
+  `};
 `;
 
 const Heading = styled.h1`
-  margin: 0 0 15px;
+  margin: 0px 0 17px;
 
-  font-family: 'GraphikLCG-Medium';
-  font-size: 25px;
-  line-height: 35px;
+  font-size: 20px;
+  line-height: 25px;
+
+  ${media.sm`
+    margin: 6px 0 25px;
+    
+    font-family: 'GraphikLCG-Medium'; 
+    font-size: 25px;
+    line-height: 35px;
+  `};
 `;
 
 const Total = styled.p`
-  margin: 0 0 19px;
+  margin: 0 0 10px;
 
-  font-size: 18px;
-  line-height: 25px;
+  font-size: 15px;
+  line-height: 20px;
+
+  ${media.sm`
+    margin: 0 0 26px;
+    font-size: 18px;
+    line-height: 25px;
+  `};
 `;
 
 const Price = styled.span`
+  position: relative;
   color: #4285f4;
 `;
 
 const Description = styled.p`
-  margin: 0 auto;
-  max-width: 375px;
+  max-width: 435px;
+  margin: 0 0 17px;
 
-  font-size: 14px;
-  line-height: 19px;
+  font-size: 15px;
+  line-height: 20px;
+
+  ${media.sm`
+    margin: 0 auto 28px;
+    font-size: 14px;
+    line-height: 19px;
+  `};
 `;
 
-const Compensation = Description.extend`
+const Compensation = styled(Description)`
   position: relative;
+  max-width: 375px;
+  margin-bottom: 10px;
+
+  ${media.sm`
+    margin-bottom: 15px;
+  `};
 `;
 
 export default () => (
@@ -47,7 +80,10 @@ export default () => (
     </Total>
     <Compensation>
       Размер гарантированной компенсации «ПИК Аренда» при условии соблюдения условий договора:
-      <Price> 1000 руб.</Price> <Question />
+      <Price>
+        1000 руб.
+        <Question text="Размер гарантии определяется в процентном отношении к лицензионному вознаграждению" />
+      </Price>
     </Compensation>
     <Description>
       После оплаты вам необходимо оплатить первый месяц аренды напрямую по реквизитам собственника
--- src/Main/index.js
+++ src/Main/index.js
@@ -1,13 +1,31 @@
 import React, { Fragment } from 'react';
+import styled from 'styled-components';
+
+import media from '../style-utils';
 
 import Header from './Header/';
+import Close from '../ui/Close';
 import Text from './Text';
 import Form from './Form/';
+import Footer from './Footer/';
+
+const Main = styled.main`
+  position: relative;
+  padding-top: 21px;
+
+  ${media.sm`
+   padding-top: 56px;
+  `};
+`;
 
 export default () => (
   <Fragment>
     <Header />
-    <Text />
-    <Form />
+    <Main>
+      <Close />
+      <Text />
+      <Form />
+    </Main>
+    <Footer />
   </Fragment>
 );
